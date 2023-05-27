e987b70326fcd03f06e58a757876cbbb6c303044
inpink <inpink@kakao.com>
2023-05-28 01:57:09 +0900

    Notice Model 생성

--- src/main/java/boardMC/Notice.java
+++ src/main/java/boardMC/Notice.java
@@ -3,6 +3,64 @@ package boardMC;
 //MVC 중 Model을 담당함
 public class Notice{
 	
+	private int aid;
+	private String name;
+	private String email;
+	private String date;
+	private String title;
+	private String pwd;
+	private String content;
+	
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
 	
 	
 	
